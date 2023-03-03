b90cd6d95ff9dea030d08177bac80f990c58ad9f
Red Chien <redchien@viridiasolutions.com>
2023-03-03 11:29:10 +0800

    done

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,16 +1,25 @@
 import { NumbersCollection } from "./NumbersCollection";
 
-export class Sorter {
-    constructor(public collection: NumbersCollection) {}
+interface Sortable {
+    length: number;
+    compare(leftIndex: number, rightIndex: number): boolean;
+    swap(leftIndex: number, rightIndex: number): void;
+}
+
+export abstract class Sorter {
+    // constructor(public collection: Sortable) {}
+    abstract length: number
+    abstract compare(lefIndex: number, rightIndex: number): boolean
+    abstract swap(lefIndex: number, rightIndex: number): void
 
     sort(): void {
-        const { length } = this.collection;
+        const { length } = this;
 
         for (let i = 0; i < length; i++) {
             let isSwap = false;
             for (let j = 0; j < length - i - 1; j++) {
-                if(this.collection.compare(j, j + 1)){
-                    this.collection.swap(j, j + 1)
+                if(this.compare(j, j + 1)){
+                    this.swap(j, j + 1)
                     isSwap = true
                 }
             }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0, 2, 8, 7])
-const sorter = new Sorter(numbersCollection);
+numbersCollection.sort()
 
-console.log("sorter instanceof Sorter :>> ", sorter instanceof Sorter);
-sorter.sort();
-console.log('Result: >> ', numbersCollection.data);
+console.log('numbersCollection.data :>> ', numbersCollection.data);
+// const sorter = new Sorter(numbersCollection);
+
+// console.log("sorter instanceof Sorter :>> ", sorter instanceof Sorter);
+// sorter.sort();
+// console.log('Result: >> ', numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('aaXXuByC')
+charactersCollection.sort()
+
+console.log('charactersCollection.data :>> ', charactersCollection.data);
+// const sorter2 = new Sorter(charactersCollection)
+// sorter2.sort()
+// console.log('charactersCollection.data :>> ', charactersCollection.data);
+
+const linkedList = new LinkedList()
+linkedList.add(1)
+linkedList.add(2)
+linkedList.add(-500)
+linkedList.add(0)
+
+linkedList.sort()
+linkedList.print()
+// const linkedList = new Sorter(new LinkedList())
+// linkedList.compare(0, 1)
+
+
+// const sorter3 = new Sorter(linkedList)
+// sorter3.sort()
+// linkedList.print()
